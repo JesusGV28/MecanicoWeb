7ce018440f8f8f838421cc75472e08a1e93bb8fd
JesusGV28 <s23120183@alumnos.itsur.edu.mx>
2026-03-01 16:31:39 -0600

    Add files via upload

---
+++ Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TallerMecanicoWeb.backend.Coneccion
+{
+    public class Conexion
+    {
+        private SqlConnection conexion;
+
+        public Conexion()
+        {
+            // Leemos la cadena de conexión directamente del Web.config de forma segura
+            string cadena = ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString;
+            conexion = new SqlConnection(cadena);
+        }
+
+        public bool AbrirConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Closed)
+                {
+                    conexion.Open();
+                    return true;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Si falla, aquí sabremos por qué
+                throw new Exception("Error al abrir conexión: " + ex.Message);
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion != null && conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            return conexion;
+        }
+    }
+}
